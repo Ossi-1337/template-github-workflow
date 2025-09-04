255bef0ada8d9ce438c09d81a79d0f86f5db1b93
Ossi <oskarolsen7@gmail.com>
2025-09-04 10:44:13 +0200

    update

--- .github/scripts/commit-graph.py
+++ .github/scripts/commit-graph.py
@@ -1,3 +1,13 @@
-# print out all args when this script runs
-import sys
-print("Args:", sys.argv[1:])
+import pandas as pd
+import matplotlib.pyplot as plt
+
+df = pd.read_csv('.github/data/commit.csv', parse_dates=['Date'])
+
+plt.figure(figsize=(10, 6))
+df['Date'].value_counts().sort_index().plot(kind='bar')
+plt.xlabel('Date')
+plt.ylabel('Number of Commits')
+plt.title('Commits count by Date')
+plt.tight_layout()
+plt.savefig('.github/data/commit_graph.png')
+plt.show()
